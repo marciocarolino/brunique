7f670305857d954d7b38a2ea62ee93cb9319a6f8
marcio carolino <marciocarolino@gmail.com>
2022-09-29 21:27:31 -0300

    separando os arquivos e começando a deixar o site melhor

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <img class="mb-2" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="24" height="19">
+        <small class="d-block mb-3 text-muted">&copy; 2022 BRUNIQUE SEGUROS</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Contatos</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Telefones</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">(61) 3352-6526</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">(61) 98469-6397</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+
+
+
+
+</div>
+
+
+   <script src="./src/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
  <head>
    <title>BR Unique</title>
+   <link rel="stylesheet" href="./src/bootstrap/css/bootstrap.min.css">
  </head>
    <body>
 
+   <div class="container py-3">
+  
 
-  </body>
+<?php include "header.php"; ?>
 
-</html>
+<?php include "main.php"; ?>
+
+
+<?php include "footer.php"; ?>
